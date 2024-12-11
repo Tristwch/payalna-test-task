e2ba46af639ede17ac8cd20024c6ed1646222c23
Vitalii Shevchenko <vitalii.shevchenko@calaton.systems>
2024-12-11 12:56:13 +0200

    init vue 3

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+
+  .wrapper {
+    max-width: 1200px;
+    margin: 0px auto;
+    padding: 16px;
+
+    nav {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 16px;
+
+      a {
+        text-decoration: none;
+        color: #333;
+        font-weight: bold;
+        transition: color 0.3s ease;
+
+        &:hover {
+          color: hsla(160, 100%, 37%);
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">main</main>
+</template>
+
+<style>
+.main {
+  width: 100%;
+  height: 100%;
+}
+</style>
