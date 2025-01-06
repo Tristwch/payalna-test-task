7b9cc6a2fa2830945a2bb599080d049d1dbfe4d2
Vitalii Shevchenko <vitalii.shevchenko@calaton.systems>
2025-01-06 23:50:41 +0200

    dashboard template

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="projects">
+    <div class="header">
+      <h2>Dashboard</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
